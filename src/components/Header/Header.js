5f9ef66afd45c268a95fb77bb5bb967dd135f735
sabbirzzaman <sztonmoy7@gmail.com>
2022-03-25 21:47:08 +0600

    header completed

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
+const Header = () => {
+    return (
+        <nav>
+            <h3>
+                <a href="#">Watch Cart</a>
+            </h3>
+
+            <div className="header">
+                <a href="/home">Home</a>
+                <a href="/about">About</a>
+                <a href="/shop">Shop</a>
+                <a href="/brand">Brand</a>
+            </div>
+            <div className="cart">
+                <FontAwesomeIcon icon={faCartShopping} />
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
