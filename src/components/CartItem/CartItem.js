27da55484b925f01fc5b34cb609781b879671075
sabbirzzaman <sztonmoy7@gmail.com>
2022-03-26 19:42:04 +0600

    ui update

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import './CartItem.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
 const CartItem = ({ item }) => {
     const { name, image } = item;
 
     return (
         <div className="cart-item">
-            <div className="item-info">
-                <img src={image} alt={name} />
-                <p>{name}</p>
-            </div>
-            <button className="remove-btn">
-                <FontAwesomeIcon icon={faTrash} />
-            </button>
+            <img src={image} alt={name} />
+            <p>{name}</p>
         </div>
     );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,21 +5,23 @@ import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const Header = () => {
     return (
-        <nav>
-            <h3>
-                <a href="#">Watch Cart</a>
-            </h3>
+        <div className='navigation'>
+            <nav>
+                <h3>
+                    <a href="#">Watch Cart</a>
+                </h3>
 
-            <div className="header">
-                <a href="/home">Home</a>
-                <a href="/about">About</a>
-                <a href="/shop">Shop</a>
-                <a href="/brand">Brand</a>
-            </div>
-            <div className="cart-icon">
-                <FontAwesomeIcon icon={faCartShopping} />
-            </div>
-        </nav>
+                <div className="header">
+                    <a href="/home">Home</a>
+                    <a href="/about">About</a>
+                    <a href="/shop">Shop</a>
+                    <a href="/brand">Brand</a>
+                </div>
+                <div className="cart-icon">
+                    <FontAwesomeIcon icon={faCartShopping} />
+                </div>
+            </nav>
+        </div>
     );
 };
 
