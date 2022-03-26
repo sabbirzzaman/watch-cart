23e3f285a8c755194ef12e290ed749695e1517ce
sabbirzzaman <sztonmoy7@gmail.com>
2022-03-26 20:24:09 +0600

    readme updated

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -40,6 +40,41 @@ const Shop = () => {
                         key={product.id}
                     ></Product>
                 ))}
+
+                <div className='question'>
+                    <h2>How react works?</h2>
+                    <p>
+                        React.js is a javascript library that creates user interfaces. You can use it for building single page applications or complex applications. We can say react is a cullection of javascript code that help us to 
+                    </p>
+                </div>
+                <div className='question'>
+                    <h2>Props vs State</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                </div>
+                <div className='question'>
+                    <h2>How useState works?</h2>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        exercitation ullamco laboris nisi ut aliquip ex ea
+                        commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum.
+                    </p>
+                </div>
             </div>
 
             <Cart cart={cart} cartHandle={setCart}></Cart>
